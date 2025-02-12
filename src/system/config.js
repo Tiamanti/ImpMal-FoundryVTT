840de18c18758dca20523724d9df6df5034448cc
Michał Szura <michal.szura@gmail.com>
2025-02-13 00:15:44 +0100

    refactor: Adjust to new effect system

--- src/system/config.js
+++ src/system/config.js
@@ -816,7 +816,23 @@ const IMPMAL = {
     },
 
     traitEffects : {
-       
+       loud: {
+           name: "IMPMAL.Loud",
+           system: {
+               transferData : {
+                   documentType: "Actor"
+               },
+               scriptData : [{
+                   label : "Disadvantage when Wearing Armour with Loud trait",
+                   trigger : "dialog",
+                   script : "args.disadvantage++;",
+                   options : {
+                       hideScript : "return args.data.skill !== 'stealth' || !args.data.actor.itemTypes.equipped.protection.find(i => i.system.traits.list.find(i => i.key == 'loud'))",
+                       activateScript : "return true;"
+                   }
+               }]
+           }
+       },
        defensive: {
            name : "IMPMAL.Defensive",
            system : {
@@ -834,7 +850,7 @@ const IMPMAL = {
                }]
            }
        },
-        burst: {
+        shoddy: {
             name : "IMPMAL.Shoddy",
             system : {
                 transferData : {
